8cea2626c5fa90b218394cf43e1875122a27ad2b
Andrew Daniel <160974398+andrewdanieldev@users.noreply.github.com>
2025-12-16 16:58:41 +0200

    Feat/screen render dev (#22)
    * Release: v18.0.0
    
    * feat: Add missing Java API and Pigeon files
    
    - Add LuciqApi.java with complete Instabug to Luciq migration
    - Add luciq.api.dart with LuciqHostApi and LuciqFlutterApi
    - Add apm.api.dart with screen render functionality
    - Add screen_render_page.dart example page
    - Add animated_box.dart example component
    - Complete manual migration of all missing files from Instabug-Flutter branch
    
    * feat: Screen render
    
    * feat: Screen render
    
    * update native dependencies
    
    * fix linter & android tests
    
    * fix unit test
    
    * rebase
    
    * add debug logs
    
    * update: native dependencies & remove debug logs
    
    * update: remove debug logs
    
    * fix lints and failed unit tests
    
    * fix: V2.10.5 tests
    
    * amend: luciq_screen_render_manager_test_manual_mocks.dart to work with Flutter V3
    
    * Release: 18.2.1 (#27)
    
    * feat: enabling network automasking (#6)
    
    * Release: v18.0.0
    
    * feat: enabling network automasking
    
    * fix: ci
    
    * feat: proactive bug-reporting (#4)
    
    * Release: v18.0.0
    
    * feat: proactive bug-reporting
    
    * fix: ci
    
    * Release: 18.0.1 (#10)
    
    * feat: support NDK crash (#2)
    
    * Release: v18.0.0
    
    * feat: support NDK crash
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * Feat: add patrol e2e (#7)
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * feat(cli): add command to upload symbol files (#15)
    
    * feat(cli): add command to upload symbol files
    
    * feat(cli): add command to upload symbol files
    
    * feat(cli): add command to upload symbol files
    
    * feat(cli): add command to upload symbol files
    
    * feat(cli): add command to upload symbol files
    
    * feat: Make LuciqNavigatorObserver screen report delay configurable. (#17)
    
    * feat: Make LuciqNavigatorObserver screen report delay configurable.
    
    * add change log
    
    * Release:18.2.0 (#16)
    
    * Release: 18.0.1 (#11)
    
    * feat: enabling network automasking (#6)
    
    * Release: v18.0.0
    
    * feat: enabling network automasking
    
    * fix: ci
    
    * feat: proactive bug-reporting (#4)
    
    * Release: v18.0.0
    
    * feat: proactive bug-reporting
    
    * fix: ci
    
    * Release: 18.0.1 (#10)
    
    * feat: support NDK crash (#2)
    
    * Release: v18.0.0
    
    * feat: support NDK crash
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * Feat: add patrol e2e (#7)
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * Release: 18.0.1
    
    * Release: 18.2.0
    
    * Release: 18.2.0
    
    * Release: 18.2.0
    
    * Release: 18.2.0
    
    * Release: 18.2.0
    
    * Release: 18.2.0
    
    * fix: Guard LuciqNavigatorObserver pending-step removal to eliminate the race that could crash apps.
    
    * add change log
    
    * release: 18.2.1
    
    * release: 18.2.1
    
    * release: 18.2.1
    
    ---------
    
    Co-authored-by: AyaMahmoud148 <ayaabdelbaseet@instabug.com>
    
    * update native dependencies
    
    * release: 19.0.0
    
    * release: 19.0.0
    
    * release: 19.0.0
    
    * release: 19.0.0
    
    ---------
    
    Co-authored-by: ahmed alaa <154802748+ahmedAlaaInstabug@users.noreply.github.com>
    Co-authored-by: AyaMahmoud148 <ayaabdelbaseet@instabug.com>
    Co-authored-by: ahmed alaa <aalaa@luciq.ai>

---
+++ packages/luciq_flutter/test/utils/screen_render/luciq_widget_binding_observer_test.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:luciq_flutter/luciq_flutter.dart';
+import 'package:luciq_flutter/src/generated/apm.api.g.dart';
+import 'package:luciq_flutter/src/utils/lcq_build_info.dart';
+import 'package:luciq_flutter/src/utils/screen_loading/ui_trace.dart';
+import 'package:luciq_flutter/src/utils/screen_name_masker.dart';
+import 'package:luciq_flutter/src/utils/screen_rendering/luciq_screen_render_manager.dart';
+import 'package:luciq_flutter/src/utils/screen_rendering/luciq_widget_binding_observer.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'luciq_widget_binding_observer_test.mocks.dart';
+
+@GenerateMocks([
+  LuciqScreenRenderManager,
+  ScreenLoadingManager,
+  ScreenNameMasker,
+  UiTrace,
+  ApmHostApi,
+  LCQBuildInfo,
+])
+void main() {
+  late MockLuciqScreenRenderManager mockRenderManager;
+  late MockScreenLoadingManager mockLoadingManager;
+  late MockScreenNameMasker mockNameMasker;
+  late MockUiTrace mockUiTrace;
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    mockRenderManager = MockLuciqScreenRenderManager();
+    mockLoadingManager = MockScreenLoadingManager();
+    mockNameMasker = MockScreenNameMasker();
+    mockUiTrace = MockUiTrace();
+
+    // Inject singleton mocks
+    LuciqScreenRenderManager.setInstance(mockRenderManager);
+    ScreenLoadingManager.setInstance(mockLoadingManager);
+    ScreenNameMasker.setInstance(mockNameMasker);
+  });
+
+  group('LuciqWidgetsBindingObserver', () {
+    final mApmHost = MockApmHostApi();
+    final mIBGBuildInfo = MockLCQBuildInfo();
+    setUp(() {
+      APM.$setHostApi(mApmHost);
+      LCQBuildInfo.setInstance(mIBGBuildInfo);
+    });
+
+    test('returns the singleton instance', () {
+      final instance = LuciqWidgetsBindingObserver.instance;
+      final shorthand = LuciqWidgetsBindingObserver.I;
+      expect(instance, isA<LuciqWidgetsBindingObserver>());
+      expect(shorthand, same(instance));
+    });
+
+    test('handles AppLifecycleState.resumed and starts UiTrace', () async {
+      when(mockLoadingManager.currentUiTrace).thenReturn(mockUiTrace);
+      when(mockUiTrace.screenName).thenReturn("HomeScreen");
+      when(mockNameMasker.mask("HomeScreen")).thenReturn("MaskedHome");
+      when(mockLoadingManager.startUiTrace("MaskedHome", "HomeScreen"))
+          .thenAnswer((_) async => 123);
+      when(mockRenderManager.screenRenderEnabled).thenReturn(true);
+
+      when(mApmHost.isScreenRenderEnabled()).thenAnswer((_) async => true);
+
+      LuciqWidgetsBindingObserver.I
+          .didChangeAppLifecycleState(AppLifecycleState.resumed);
+
+      // wait for async call to complete
+      await untilCalled(
+        mockRenderManager.startScreenRenderCollectorForTraceId(123),
+      );
+
+      verify(mockRenderManager.startScreenRenderCollectorForTraceId(123))
+          .called(1);
+    });
+
+    test(
+        'handles AppLifecycleState.paused and stops render collector for iOS platform',
+        () {
+      when(mockRenderManager.screenRenderEnabled).thenReturn(true);
+      when(mIBGBuildInfo.isIOS).thenReturn(true);
+
+      LuciqWidgetsBindingObserver.I
+          .didChangeAppLifecycleState(AppLifecycleState.paused);
+
+      verify(mockRenderManager.syncCollectedScreenRenderingData()).called(1);
+    });
+
+    test('handles AppLifecycleState.inactive with no action', () {
+      // Just ensure it doesn't crash
+      expect(
+        () {
+          LuciqWidgetsBindingObserver.I
+              .didChangeAppLifecycleState(AppLifecycleState.inactive);
+        },
+        returnsNormally,
+      );
+    });
+
+    test('_handleResumedState does nothing if no currentUiTrace', () {
+      when(mockLoadingManager.currentUiTrace).thenReturn(null);
+
+      LuciqWidgetsBindingObserver.I
+          .didChangeAppLifecycleState(AppLifecycleState.resumed);
+
+      verifyNever(mockRenderManager.startScreenRenderCollectorForTraceId(any));
+    });
+
+    test('checkForWidgetBinding ensures initialization', () {
+      expect(() => checkForWidgetBinding(), returnsNormally);
+    });
+  });
+}

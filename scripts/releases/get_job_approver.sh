9707cd392a3de17cdd11df2c62c022d3eb635612
Mohamed Kamal <m.kamalgamil@gmail.com>
2025-12-17 09:50:07 +0200

    fix: update job approval logic in release script to work on release/test approval jobs and add error handling

--- scripts/releases/get_job_approver.sh
+++ scripts/releases/get_job_approver.sh
@@ -1,6 +1,11 @@
 jobsJson=$(curl -s -X GET "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/job" --header "Circle-Token: $CIRCLE_TOKEN")
 
-job=$(jq '.items[] | select(.name == "hold_release_slack_notification")' <<< "$jobsJson")
+job=$(jq '.items[] | select(.name == "hold_release_luciq_flutter" or .name == "hold_slack_notification") | select(.approved_by != null)' <<< "$jobsJson")
+
+if [ -z "$job" ] || [ "$job" == "null" ]; then
+  echo "Error: Could not find approved job in workflow" >&2
+  exit 1
+fi
 
 approver_id=$(jq '.approved_by' <<< "$job")
 

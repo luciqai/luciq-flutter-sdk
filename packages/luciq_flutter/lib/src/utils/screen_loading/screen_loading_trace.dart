c06eb6046429e89a920e149124fef1683b0f38cf
Andrew Daniel <adaniel@luciq.ai>
2026-04-06 13:52:07 +0200

    fix: operator == overriding consistency

--- packages/luciq_flutter/lib/src/utils/screen_loading/screen_loading_trace.dart
+++ packages/luciq_flutter/lib/src/utils/screen_loading/screen_loading_trace.dart
@@ -63,7 +63,5 @@ class ScreenLoadingTrace {
         screenName,
         startTimeInMicroseconds,
         startMonotonicTimeInMicroseconds,
-        endTimeInMicroseconds,
-        duration,
       );
 }

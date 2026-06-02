281e43f80a8d62cd4daed74c08ee923bc61ec52c
Andrew Daniel <adaniel@luciq.ai>
2026-06-02 21:29:38 +0300

    feat(ios): instrument FeatureRequestsApi entry logs

--- packages/luciq_flutter/ios/Classes/Modules/FeatureRequestsApi.m
+++ packages/luciq_flutter/ios/Classes/Modules/FeatureRequestsApi.m
@@ -1,6 +1,8 @@
 #import "LuciqSDK/LuciqSDK.h"
 #import "FeatureRequestsApi.h"
 #import "ArgsRegistry.h"
+#import "../Util/LuciqFlutterLogger.h"
+#import "../Util/LuciqFlutterDebugTags.h"
 
 extern void InitFeatureRequestsApi(id<FlutterBinaryMessenger> messenger) {
     FeatureRequestsApi *api = [[FeatureRequestsApi alloc] init];
@@ -10,10 +12,12 @@ extern void InitFeatureRequestsApi(id<FlutterBinaryMessenger> messenger) {
 @implementation FeatureRequestsApi
 
 - (void)showWithError:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags featureRequests] format:@"[FR.show]"];
     [LCQFeatureRequests show];
 }
 
 - (void)setEmailFieldRequiredIsRequired:(NSNumber *)isRequired actionTypes:(NSArray<NSString *> *)actionTypes error:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags featureRequests] format:@"[FR.setEmailFieldRequired] isRequired=%@ actionTypesCount=%lu", ([isRequired boolValue] ? @"YES" : @"NO"), (unsigned long)actionTypes.count];
     LCQAction resolvedTypes = 0;
 
     for (NSString *type in actionTypes) {

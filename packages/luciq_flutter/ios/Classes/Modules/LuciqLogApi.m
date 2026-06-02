a1eec86d8b6bc07394afe01d0dd2c55f47143593
Andrew Daniel <adaniel@luciq.ai>
2026-06-02 21:34:12 +0300

    feat(ios): instrument LuciqLogApi entry logs

--- packages/luciq_flutter/ios/Classes/Modules/LuciqLogApi.m
+++ packages/luciq_flutter/ios/Classes/Modules/LuciqLogApi.m
@@ -1,5 +1,7 @@
 #import "LuciqSDK/LuciqSDK.h"
 #import "LuciqLogApi.h"
+#import "../Util/LuciqFlutterLogger.h"
+#import "../Util/LuciqFlutterDebugTags.h"
 
 extern void InitLuciqLogApi(id<FlutterBinaryMessenger> messenger) {
     LuciqLogApi *api = [[LuciqLogApi alloc] init];
@@ -9,26 +11,32 @@ extern void InitLuciqLogApi(id<FlutterBinaryMessenger> messenger) {
 @implementation LuciqLogApi
 
 - (void)logVerboseMessage:(NSString *)message error:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags luciqLog] format:@"[LCQLog.logVerbose] length=%lu", (unsigned long)message.length];
     [LCQLog logVerbose:message];
 }
 
 - (void)logDebugMessage:(NSString *)message error:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags luciqLog] format:@"[LCQLog.logDebug] length=%lu", (unsigned long)message.length];
     [LCQLog logDebug:message];
 }
 
 - (void)logInfoMessage:(NSString *)message error:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags luciqLog] format:@"[LCQLog.logInfo] length=%lu", (unsigned long)message.length];
     [LCQLog logInfo:message];
 }
 
 - (void)logWarnMessage:(NSString *)message error:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags luciqLog] format:@"[LCQLog.logWarn] length=%lu", (unsigned long)message.length];
     [LCQLog logWarn:message];
 }
 
 - (void)logErrorMessage:(NSString *)message error:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags luciqLog] format:@"[LCQLog.logError] length=%lu", (unsigned long)message.length];
     [LCQLog logError:message];
 }
 
 - (void)clearAllLogsWithError:(FlutterError *_Nullable __autoreleasing *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags luciqLog] format:@"[LCQLog.clearAllLogs]"];
     [LCQLog clearAllLogs];
 }
 

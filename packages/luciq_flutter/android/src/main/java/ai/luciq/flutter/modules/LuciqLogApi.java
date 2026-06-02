93f7edb180ef8c4cd737b2a90c19fa8b3cb3fdde
Andrew Daniel <adaniel@luciq.ai>
2026-06-02 20:57:38 +0300

    feat(android): instrument LuciqLogApi entry logs

--- packages/luciq_flutter/android/src/main/java/ai/luciq/flutter/modules/LuciqLogApi.java
+++ packages/luciq_flutter/android/src/main/java/ai/luciq/flutter/modules/LuciqLogApi.java
@@ -3,6 +3,8 @@ package ai.luciq.flutter.modules;
 import androidx.annotation.NonNull;
 
 import ai.luciq.flutter.generated.LuciqLogPigeon;
+import ai.luciq.flutter.util.LuciqFlutterDebugTags;
+import ai.luciq.flutter.util.LuciqFlutterLogger;
 import ai.luciq.library.logging.LuciqLog;
 
 import io.flutter.plugin.common.BinaryMessenger;
@@ -15,31 +17,37 @@ public class LuciqLogApi implements LuciqLogPigeon.LuciqLogHostApi {
 
     @Override
     public void logVerbose(@NonNull String message) {
+        LuciqFlutterLogger.d(LuciqFlutterDebugTags.LUCIQ_LOG, "[logVerbose] length=" + message.length());
         LuciqLog.v(message);
     }
 
     @Override
     public void logDebug(@NonNull String message) {
+        LuciqFlutterLogger.d(LuciqFlutterDebugTags.LUCIQ_LOG, "[logDebug] length=" + message.length());
         LuciqLog.d(message);
     }
 
     @Override
     public void logInfo(@NonNull String message) {
+        LuciqFlutterLogger.d(LuciqFlutterDebugTags.LUCIQ_LOG, "[logInfo] length=" + message.length());
         LuciqLog.i(message);
     }
 
     @Override
     public void logWarn(@NonNull String message) {
+        LuciqFlutterLogger.d(LuciqFlutterDebugTags.LUCIQ_LOG, "[logWarn] length=" + message.length());
         LuciqLog.w(message);
     }
 
     @Override
     public void logError(@NonNull String message) {
+        LuciqFlutterLogger.d(LuciqFlutterDebugTags.LUCIQ_LOG, "[logError] length=" + message.length());
         LuciqLog.e(message);
     }
 
     @Override
     public void clearAllLogs() {
+        LuciqFlutterLogger.d(LuciqFlutterDebugTags.LUCIQ_LOG, "[clearAllLogs]");
         LuciqLog.clearLogs();
     }
 }

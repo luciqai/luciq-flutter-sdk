b872deac16933e3635c5c6b1c2545e7e20ecffd6
Andrew Daniel <adaniel@luciq.ai>
2026-06-02 21:30:34 +0300

    feat(ios): instrument RepliesApi entry logs

--- packages/luciq_flutter/ios/Classes/Modules/RepliesApi.m
+++ packages/luciq_flutter/ios/Classes/Modules/RepliesApi.m
@@ -1,5 +1,7 @@
 #import "LuciqSDK/LuciqSDK.h"
 #import "RepliesApi.h"
+#import "../Util/LuciqFlutterLogger.h"
+#import "../Util/LuciqFlutterDebugTags.h"
 
 extern void InitRepliesApi(id<FlutterBinaryMessenger> messenger) {
     RepliesFlutterApi *flutterApi = [[RepliesFlutterApi alloc] initWithBinaryMessenger:messenger];
@@ -16,32 +18,39 @@ extern void InitRepliesApi(id<FlutterBinaryMessenger> messenger) {
 }
 
 - (void)setEnabledIsEnabled:(NSNumber *)isEnabled error:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags replies] format:@"[Replies.setEnabled] isEnabled=%@", ([isEnabled boolValue] ? @"YES" : @"NO")];
     BOOL boolValue = [isEnabled boolValue];
     LCQReplies.enabled = boolValue;
 }
 
 - (void)showWithError:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags replies] format:@"[Replies.show]"];
     [LCQReplies show];
 }
 
 - (void)setInAppNotificationsEnabledIsEnabled:(NSNumber *)isEnabled error:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags replies] format:@"[Replies.setInAppNotificationsEnabled] isEnabled=%@", ([isEnabled boolValue] ? @"YES" : @"NO")];
     BOOL boolValue = [isEnabled boolValue];
     LCQReplies.inAppNotificationsEnabled = boolValue;
 }
 
 - (void)setInAppNotificationSoundIsEnabled:(NSNumber *)isEnabled error:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags replies] format:@"[Replies.setInAppNotificationSound] iOS noop"];
     // Android Only
 }
 
 - (void)getUnreadRepliesCountWithCompletion:(nonnull void (^)(NSNumber * _Nullable, FlutterError * _Nullable))completion {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags replies] format:@"[Replies.getUnreadRepliesCount]"];
     completion([NSNumber numberWithLong:LCQReplies.unreadRepliesCount], nil);
 }
 
 - (void)hasChatsWithCompletion:(nonnull void (^)(NSNumber * _Nullable, FlutterError * _Nullable))completion {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags replies] format:@"[Replies.hasChats]"];
     completion([NSNumber numberWithBool:LCQReplies.hasChats], nil);
 }
 
 - (void)bindOnNewReplyCallbackWithError:(FlutterError *_Nullable *_Nonnull)error {
+    [LuciqFlutterLogger d:[LuciqFlutterDebugTags replies] format:@"[Replies.bindOnNewReplyCallback]"];
     LCQReplies.didReceiveReplyHandler = ^{
       [self->_flutterApi onNewReplyWithCompletion:^(FlutterError *_Nullable _){
       }];

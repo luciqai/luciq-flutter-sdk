f7d68850576170b3216de375ca4cd368e0b1413c
Andrew Daniel <adaniel@luciq.ai>
2026-06-02 18:52:25 +0300

    feat(luciq-log): instrument LuciqLog APIs with length-only debug logs

--- packages/luciq_flutter/lib/src/modules/luciq_log.dart
+++ packages/luciq_flutter/lib/src/modules/luciq_log.dart
@@ -2,7 +2,9 @@
 
 import 'dart:async';
 
+import 'package:luciq_flutter/src/constants/debug_tags.dart';
 import 'package:luciq_flutter/src/generated/luciq_log.api.g.dart';
+import 'package:luciq_flutter/src/utils/luciq_logger.dart';
 import 'package:meta/meta.dart';
 
 class LuciqLog {
@@ -20,6 +22,10 @@ class LuciqLog {
   /// All log messages are timestamped
   /// Note: logs passed to this method are NOT printed to console
   static Future<void> logVerbose(String message) async {
+    LuciqLogger.I.d(
+      'logVerbose length=${message.length}',
+      tag: DebugTags.luciqLog,
+    );
     return _host.logVerbose(message);
   }
 
@@ -28,6 +34,10 @@ class LuciqLog {
   /// All log messages are timestamped
   /// Note: logs passed to this method are NOT printed to console
   static Future<void> logDebug(String message) async {
+    LuciqLogger.I.d(
+      'logDebug length=${message.length}',
+      tag: DebugTags.luciqLog,
+    );
     return _host.logDebug(message);
   }
 
@@ -36,6 +46,10 @@ class LuciqLog {
   /// All log messages are timestamped
   /// Note: logs passed to this method are NOT printed to console
   static Future<void> logInfo(String message) async {
+    LuciqLogger.I.d(
+      'logInfo length=${message.length}',
+      tag: DebugTags.luciqLog,
+    );
     return _host.logInfo(message);
   }
 
@@ -44,6 +58,10 @@ class LuciqLog {
   /// All log messages are timestamped
   /// Note: logs passed to this method are NOT printed to console
   static Future<void> logWarn(String message) async {
+    LuciqLogger.I.d(
+      'logWarn length=${message.length}',
+      tag: DebugTags.luciqLog,
+    );
     return _host.logWarn(message);
   }
 
@@ -52,11 +70,16 @@ class LuciqLog {
   /// All log messages are timestamped
   /// Note: logs passed to this method are NOT printed to console
   static Future<void> logError(String message) async {
+    LuciqLogger.I.d(
+      'logError length=${message.length}',
+      tag: DebugTags.luciqLog,
+    );
     return _host.logError(message);
   }
 
   /// Clears Luciq internal log
   static Future<void> clearAllLogs() async {
+    LuciqLogger.I.d('clearAllLogs invoked', tag: DebugTags.luciqLog);
     return _host.clearAllLogs();
   }
 }
